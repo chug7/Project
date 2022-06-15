114e3258d14777bce00f5efebc5cfb18de2f0cf6
bkim <merntest1234@gmail.com>
2022-06-15 22:16:23 +1000

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import './App.css'
 
 const App = () => (
   //changed "function App()" into an arrow function
