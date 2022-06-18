b1b82b09756ca8159adcae1ab4244c177f09fd4a
bkim <merntest1234@gmail.com>
2022-06-18 14:40:19 +1000

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React, { Fragment } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 import './App.css'
 
 const App = () => (
   //changed "function App()" into an arrow function
-
-  <Fragment>
-    <Navbar /> <Landing />
-  </Fragment>
+  <BrowserRouter>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+      </Routes>
+      <section className="container">
+        <Routes>
+          <Route exact path="/register.html" element={<Register />} />
+          <Route exact path="/login.html" element={<Login />} />
+        </Routes>
+      </section>
+    </Fragment>
+  </BrowserRouter>
 )
 
 export default App
