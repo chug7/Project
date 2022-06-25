d01deb51d785ec8d834fdffbdb7b54c5500bbbfd
bkim <merntest1234@gmail.com>
2022-06-26 00:48:00 +1000

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,28 @@ import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import './App.css'
+//Redux
+import { Provider } from 'react-redux'
+import store from './store'
 
 const App = () => (
   //changed "function App()" into an arrow function
-  <BrowserRouter>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-      </Routes>
-      <section className="container">
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
         <Routes>
-          <Route exact path="register" element={<Register />} />
-          <Route exact path="login" element={<Login />} />
+          <Route exact path="/" element={<Landing />} />
         </Routes>
-      </section>
-    </Fragment>
-  </BrowserRouter>
+        <section className="container">
+          <Routes>
+            <Route exact path="register" element={<Register />} />
+            <Route exact path="login" element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 )
 
 export default App
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const Login = () => {
+const Register = () => {
   const [formData, setFormData] = useState({
     email: '',
-    password: ''
+    password: '',
   })
 
   const { email, password } = formData // (object destructuring)
@@ -15,15 +15,14 @@ const Login = () => {
 
   const onSubmit = async (e) => {
     e.preventDefault()
-      console.log('SUCCESS')
-     }
+    console.log('SUCCESS')
   }
 
   return (
     <Fragment>
-      <h1 className="large text-primary">Sign In</h1>
+      <h1 className="large text-primary">Sign Up</h1>
       <p className="lead">
-        <i className="fas fa-user"></i> Sign Into Your Account
+        <i className="fas fa-user"></i> Create Your Account
       </p>
       <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
@@ -47,13 +46,13 @@ const Login = () => {
             required
           />
         </div>
-        <input type="submit" className="btn btn-primary" value="Login" />
+        <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Don't have an account? <Link to='/register'>Sign Up</Link>
+        Don't have an account? <Link to="/Register">Register</Link>
       </p>
     </Fragment>
   )
 }
 
-export default Login
+export default Register
